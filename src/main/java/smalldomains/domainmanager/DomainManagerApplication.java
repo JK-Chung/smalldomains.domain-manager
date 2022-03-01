054e0be5ec1421493062adfdff619dd74ad9c5d2
JK-Chung <chungjeeken@outlook.com>
2022-03-01 18:56:05 +0000

    first commit

---
+++ src/main/java/smalldomains/domainmanager/DomainManagerApplication.java
@@ -0,0 +1,13 @@
+package smalldomains.domainmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smalldomains/domainmanager/DomainManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package smalldomains.domainmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
