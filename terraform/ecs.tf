682f4dd008d2d8a2b35a103598d0561c1a8954fe
JK-Chung <chungjeeken@outlook.com>
2022-05-28 22:11:56 +0100

    removed public IP

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -27,7 +27,6 @@ resource "aws_ecs_service" "domain-manager" {
   network_configuration {
     subnets          = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
     security_groups  = [data.aws_ssm_parameter.sg_for_ecs_services.value]
-    assign_public_ip = true
   }
 
   health_check_grace_period_seconds = 30
