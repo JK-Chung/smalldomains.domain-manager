6e26d358d0368f11b060c44b4e36b95bfaf1ca5e
JK-Chung <chungjeeken@outlook.com>
2022-07-08 19:09:02 +0100

    Adding logs for health checks

--- src/main/java/smalldomains/domainmanager/health/DynamoDBTableHealthIndicator.java
+++ src/main/java/smalldomains/domainmanager/health/DynamoDBTableHealthIndicator.java
@@ -1,11 +1,14 @@
 package smalldomains.domainmanager.health;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import smalldomains.domainmanager.repository.SmallDomainRepository;
 
+@Slf4j
 @Component
 public class DynamoDBTableHealthIndicator implements ReactiveHealthIndicator {
 
@@ -19,7 +22,15 @@ public class DynamoDBTableHealthIndicator implements ReactiveHealthIndicator {
     public Mono<Health> health() {
         return Mono.fromFuture(repository.isTableStillAvailable())
                 .map(isTableStillAvailable -> isTableStillAvailable ? Health.up() : Health.down())
-                .map(Health.Builder::build);
+                .map(Health.Builder::build)
+                .doOnSuccess(health -> {
+                    if(health.getStatus().equals(Status.UP)) {
+                        log.info("Domain-Manager is Healthy: {}", health);
+                    } else {
+                        log.error("Domain-Manager is Unhealthy: {}", health);
+                    }
+                })
+                .doOnError(error -> log.error("Domain-Manager is unhealthy with error: ", error));
     }
 
 }
