55af4c4ae3195c8af936c72474e19cc33bcd9ab4
JK-Chung <chungjeeken@outlook.com>
2022-09-24 14:41:44 +0100

    SDOMAINS-4 cleaning up

--- src/main/java/smalldomains/domainmanager/validationConstraints/DomainValidator.java
+++ src/main/java/smalldomains/domainmanager/validationConstraints/DomainValidator.java
@@ -1,7 +1,6 @@
 package smalldomains.domainmanager.validationConstraints;
 
-import io.netty.util.internal.StringUtil;
-import org.apache.commons.validator.UrlValidator;
+import org.apache.commons.validator.routines.UrlValidator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -13,48 +12,46 @@ import java.net.URL;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import static org.apache.commons.validator.routines.DomainValidator.ArrayType.GENERIC_PLUS;
 
 public class DomainValidator implements ConstraintValidator<ValidDomain, String> {
 
     private static final String IANA_VALID_TLDS_FILENAME = "/iana_valid_domains.txt";
-    private static final Pattern HAS_TLD_REGEX = Pattern.compile("[^.]+\\.[^.]+$");
-    private final Set<String> VALID_TLDS = getIANAValidTLDs();
     private final UrlValidator urlValidator = new UrlValidator();
+
+    static {
+        // to ensure this.urlValidator knows all IANA TLDs, we have to statically configure validator.routines.DomainValidator
+        // https://stackoverflow.com/questions/35757298/adding-domain-to-urlvalidator-in-apache-commons
+        org.apache.commons.validator.routines.DomainValidator.updateTLDOverride(GENERIC_PLUS, getIANAValidTLDs().toArray(String[]::new));
+    }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if(value == null || value.startsWith("ftp://")) {
             return false;
         }
-
         final String toTest = value.startsWith("https://") || value.startsWith("http://") ? value : "https://" + value;
 
         try {
-            final URL url = new URL(toTest);
-            return urlValidator.isValid(toTest) && hasValidTLD(url);
+            new URL(toTest);
+            return urlValidator.isValid(toTest);
         } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            return false;
         }
     }
 
-    private boolean hasValidTLD(final URL url) {
-        final String[] tokenizedHost = url.getHost().split("\\.");
-        if (tokenizedHost.length == 0) return false;
-        final String tld = tokenizedHost[tokenizedHost.length - 1].toLowerCase();
-        return !StringUtil.isNullOrEmpty(tld) && VALID_TLDS.contains(tld);
-    }
-
     /**
-     * File sourced from: https://data.iana.org/TLD/tlds-alpha-by-domain.txt
+     * File sourced from: <a href="https://data.iana.org/TLD/tlds-alpha-by-domain.txt">...</a>
      * @return an authoritative collection of valid TLDs according to IANA
      */
-    private Set<String> getIANAValidTLDs() {
-        final var inputStream = Objects.requireNonNull(this.getClass().getResourceAsStream(IANA_VALID_TLDS_FILENAME));
+    private static Set<String> getIANAValidTLDs() {
+        final var inputStream = Objects.requireNonNull(DomainValidator.class.getResourceAsStream(IANA_VALID_TLDS_FILENAME));
         try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
             return bufferedReader.lines()
                     .map(String::toLowerCase)
+                    // force the use of HashSet to ensure constant lookup times... long validation times will impact user experience
                     .collect(Collectors.toCollection(HashSet::new));
         } catch (IOException e) {
             throw new IllegalStateException("Expected this file to exist in resources directory: " + IANA_VALID_TLDS_FILENAME, e);
